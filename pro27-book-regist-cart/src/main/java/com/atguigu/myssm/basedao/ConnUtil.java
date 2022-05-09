1fbf92ac9a65c20603d33e42e6d5a51e88ad81ec
wxingfu <wxffxw.664587@gmail.com>
2022-05-09 22:53:08 +0800

    20220509

---
+++ pro27-book-regist-cart/src/main/java/com/atguigu/myssm/basedao/ConnUtil.java
@@ -0,0 +1,69 @@
+package com.atguigu.myssm.basedao;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnUtil {
+
+    private static ThreadLocal<Connection> threadLocal = new ThreadLocal<>();
+
+    static Properties properties = new Properties();
+
+    static {
+        InputStream is = ConnUtil.class.getClassLoader().getResourceAsStream("jdbc.properties");
+        try {
+            properties.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Connection createConn() {
+        try {
+            DataSource druidDataSource = DruidDataSourceFactory.createDataSource(properties);
+            return druidDataSource.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // private static Connection createConn() {
+    //     try {
+    //         //1.加载驱动
+    //         Class.forName(DRIVER);
+    //         //2.通过驱动管理器获取连接对象
+    //         return DriverManager.getConnection(URL, USER, PWD);
+    //     } catch (ClassNotFoundException | SQLException e) {
+    //         e.printStackTrace();
+    //     }
+    //     return null;
+    // }
+
+    public static Connection getConn() {
+        Connection conn = threadLocal.get();
+        if (conn == null) {
+            conn = createConn();
+            threadLocal.set(conn);
+        }
+        return threadLocal.get();
+    }
+
+    public static void closeConn() throws SQLException {
+        Connection conn = threadLocal.get();
+        if (conn == null) {
+            return;
+        }
+        if (!conn.isClosed()) {
+            conn.close();
+            //threadLocal.set(null);
+            threadLocal.remove();
+        }
+    }
+}
