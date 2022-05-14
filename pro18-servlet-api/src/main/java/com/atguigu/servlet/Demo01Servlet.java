1aa7cdf318023cc6cc8efaeeec26d518be6d227f
wxingfu <wxffxw.664587@gmail.com>
2022-05-14 20:44:11 +0800

    20220514

--- pro18-servlet-api/src/main/java/com/atguigu/servlet/Demo01Servlet.java
+++ pro18-servlet-api/src/main/java/com/atguigu/servlet/Demo01Servlet.java
@@ -3,12 +3,7 @@ package com.atguigu.servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /*
 @WebServlet(urlPatterns = {"/demo01"} ,
